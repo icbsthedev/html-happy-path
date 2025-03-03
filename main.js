1d17881fa8843f21fecbc6d65cab94a574b1cab3
pawel.gonera <pawel.gonera@softnsharp.com>
2025-03-03 03:47:03 +0000

    Initial

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",
+    () => {
+      [... document
+        .getElementsByTagName("button")]
+          .forEach(btn=>btn
+            .addEventListener("click", ()=>console.log("test")));
+  });
